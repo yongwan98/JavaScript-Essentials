083be0983a9197a274c58b1603b9e1b1c6770ccf
yongwan98 <yongwan1998@gmail.com>
2022-04-08 10:52:54 +0900

    데이터타입확인 폴더 생성

---
+++ 데이터-타입-확인/main.js
@@ -0,0 +1,17 @@
+import getType from "./getType";
+
+
+console.log(typeof 123)
+console.log(typeof '가나다')
+console.log(typeof false)
+console.log(typeof null)
+console.log(typeof {})
+console.log(typeof [])
+console.log('-------------')
+
+console.log(getType(123))
+console.log(getType('가나다'))
+console.log(getType(false))
+console.log(getType(null))
+console.log(getType({}))
+console.log(getType([]))
