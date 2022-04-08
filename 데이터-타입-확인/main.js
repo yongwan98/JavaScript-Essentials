54b2a6713301cab93df7f112152b9d929ed2bbd2
yongwan98 <yongwan1998@gmail.com>
2022-04-08 11:59:58 +0900

    산술,할당 연산자 폴더 생성

--- 데이터-타입-확인/main.js
+++ 데이터-타입-확인/main.js
@@ -1,4 +1,4 @@
-import getType from "./getType";
+import getType from "../getType";
 
 
 console.log(typeof 123)
---
+++ 산술,할당-연산자/main.js
@@ -0,0 +1,14 @@
+// 산술 연산자(arithmetic operator)
+
+console.log(1 + 2)
+console.log(5 - 7)
+console.log(3 * 4)
+console.log(10 / 2)
+console.log(7 % 5)
+
+// 할당 연산자(assignment operator)
+let a = 1
+// a = a + 1
+a += 1
+
+console.log(a)
