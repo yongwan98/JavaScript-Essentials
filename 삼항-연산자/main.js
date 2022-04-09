a4e00a887bbe5b03f07ec66d133bf404efd2bfa6
yongwan98 <69830011+yongwan98@users.noreply.github.com>
2022-04-09 20:15:11 +0900

    삼항 연산자 정리

---
+++ 삼항-연산자/main.js
@@ -0,0 +1,12 @@
+// 삼항 연산자(ternary operator)
+
+const a = 1 < 2
+
+// if(a) {
+//   console.log('참')
+// } else {
+//   console.log('거짓')
+// }
+
+// if문을 사용하면 코드가 5줄이 나온다. 하지만 삼항연산자를 사용하면 한 줄로 끝나는 장점이 있다.
+console.log(a ? '참' : '거짓')
