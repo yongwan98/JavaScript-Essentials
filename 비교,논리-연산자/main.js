c7cf32448d88e2873d08a7b7ee93f7798451aba4
yongwan98 <69830011+yongwan98@users.noreply.github.com>
2022-04-09 20:02:48 +0900

    비교,논리 연산자 정리

---
+++ 비교,논리-연산자/main.js
@@ -0,0 +1,35 @@
+//  비교 연산자(comparison operator)
+// const a = 1
+// const b = 3
+
+// 일치 연산자
+// console.log(a === b)
+
+// function isEqual(x, y) {
+//   return x === y
+// }
+
+// console.log(isEqual(1,1))
+// console.log(isEqual(1,"1"))
+
+// 불일치 연산자
+// console.log(a !== b)
+
+// 꺽새기호 및 이퀄기호 사용
+// console.log(a < b)
+// console.log(a <= b)
+
+// --------------------------
+
+// 논리 연산자(logical operator)
+const a = 1 === 1
+const b = 'AB' === 'AB'
+const c = true
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+console.log('&&: ', a && b && c)
+console.log('||: ', a || b || c)
+console.log('!: ', !a)
