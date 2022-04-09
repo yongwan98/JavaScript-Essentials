c735d00d5ea3c22e08048bc81c040f889a670511
yongwan98 <69830011+yongwan98@users.noreply.github.com>
2022-04-09 20:35:00 +0900

    조건문 if else 정리

---
+++ 조건문-if-else/main.js
@@ -0,0 +1,15 @@
+import random from "./getRandom"; 
+
+// 조건문 (If statement)
+
+const a = random()
+
+if(a === 0) {
+  console.log('a is 0')
+} else if(a  === 2) {
+  console.log('a is 2')
+} else if(a  === 4) {
+  console.log('a is 4')
+} else {
+  console.log('rest...')
+}
